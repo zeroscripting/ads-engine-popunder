396e220f3b61d09c6bd7d0cf490f21f61780d423
zeroscripting <nandangbaihaqi300@gmail.com>
2026-05-26 23:28:05 +0700

    Create popunder.js

---
+++ popunder.js
@@ -0,0 +1,5 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://portslocked.com/59/69/6f/59696ffed979eb1d6b6108ba6c8de3b0.js";
+    document.head.appendChild(script);
+})();
