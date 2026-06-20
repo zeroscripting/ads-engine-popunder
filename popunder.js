c1d58e8e6ea2fede1e252fd1b633a5f5255f0b39
zeroscripting <nandangbaihaqi300@gmail.com>
2026-06-20 15:55:23 +0700

    Update popunder.js

--- popunder.js
+++ popunder.js
@@ -1,5 +1,5 @@
 (function() {
     var script = document.createElement('script');
-    script.src = "https://portslocked.com/59/69/6f/59696ffed979eb1d6b6108ba6c8de3b0.js";
+    script.src = "https://pl29812710.effectivecpmnetwork.com/9a/3e/4d/9a3e4d4e7f398415cca526fdf8407fa5.js";
     document.head.appendChild(script);
 })();
